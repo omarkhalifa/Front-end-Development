0ab63a65e255e641fcfff130f0485c45763c72a1
omarkhalifa <omarkhalifa14@gmail.com>
2021-11-03 17:17:03 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
 // JavaScript Document
+
+var Hamburger = document.querySelector("nav button");
+var MenuOmlaag = document.querySelector("body header nav:nth-of-type(2)");
+
+
+
+Hamburger.addEventListener("click", menuTonen);
+
+function menuTonen() {
+	Hamburger.classList.toggle("draai");
+	MenuOmlaag.classList.toggle("omlaag");
+}
+
+
